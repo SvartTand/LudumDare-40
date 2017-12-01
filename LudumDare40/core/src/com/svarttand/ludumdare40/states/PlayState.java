f8c0ac9181024506d74700f4c5ec3041b8a4e0a7
Blackstorm <Blackstorm@86.50.78.77>
2017-12-01 21:36:13 +0200

    Everything set up correctly for tomorrow!

---
+++ LudumDare40/core/src/com/svarttand/ludumdare40/states/PlayState.java
@@ -0,0 +1,58 @@
+package com.svarttand.ludumdare40.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.svarttand.ludumdare40.Application;
+
+
+public class PlayState extends State{
+
+	private TextureAtlas textureAtlas;
+	private Viewport viewport;
+	
+	public PlayState(GameStateManager gsm) {
+		super(gsm);
+		textureAtlas = gsm.assetManager.get("ThePack.pack", TextureAtlas.class);
+		viewport = new StretchViewport(Application.V_WIDTH, Application.V_HEIGHT, cam);
+		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+
+	@Override
+	protected void handleInput(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		batch.setProjectionMatrix(cam.combined);
+		Gdx.gl.glClearColor(0, (float) 0.6, 1, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+		
+	}
+
+}
